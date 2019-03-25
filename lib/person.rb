038eabb17ff631c562fb7d93e90ac2ea9b2e4204
Tracy Ngo <tngo1206@gmail.com>
2019-03-25 01:09:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,6 +3,6 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
     def intialize(info)
-      info.each {|attribute, data| self.send({"attribute"}=, data) }
+      info.each {|attribute, data| self.send("#{attribute}=", data) }
     end
 end
