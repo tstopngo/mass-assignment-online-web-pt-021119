d561768bf665cfa887b4d093f69222c2878d967b
Tracy Ngo <tngo1206@gmail.com>
2019-03-25 01:12:37 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
   #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  
+    def initialize(info)
+      info.each {|attribute, data| self.send(("#{attribute}="), data) }
+    end
 end
