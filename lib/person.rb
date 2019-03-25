bf46cba39360fa43387da49e487babceb0b9ed51
Tracy Ngo <tngo1206@gmail.com>
2019-03-25 01:07:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   #your code here
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
-    def intialize(attributes)
+    def intialize(info)
       attributes.each {|attribute, data|}
     end
 end
